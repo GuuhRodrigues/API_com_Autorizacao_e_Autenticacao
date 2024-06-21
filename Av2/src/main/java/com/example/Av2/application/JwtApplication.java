faab47966c3caaea9c638231d74e201ca6da0524
Gustavo Rodrigues Nascimento de Assis <12116534@aluno.newtonpaiva.br>
2024-06-21 20:21:10 -0300

    feat: ajustes do ProductRepository

--- Av2/src/main/java/com/example/Av2/application/JwtApplication.java
+++ Av2/src/main/java/com/example/Av2/application/JwtApplication.java
@@ -1,15 +1,14 @@
-package com.example.jwt_restapi.application;
+package com.example.Av2.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.example"})
-@EnableMongoRepositories("com.example.jwt_restapi.repository")
+@EnableMongoRepositories("com.example.Av2.repository")
 public class JwtApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JwtApplication.class, args);
     }
-
 }
--- Av2/src/main/java/com/example/Av2/service/ProductService.java
+++ Av2/src/main/java/com/example/Av2/service/ProductService.java
@@ -6,21 +6,19 @@ import org.springframework.stereotype.Service;
 import java.util.HashMap;
 import java.util.Map;
 
+@Service
 public class ProductService {
 
     private Map<String, Product> productRepository = new HashMap<>();
 
-    // Método para encontrar um produto pelo ID
     public Product findProductById(String id) {
         return productRepository.get(id);
     }
 
-    // Método para deletar um produto pelo ID
     public boolean deleteProductById(String id) {
         return productRepository.remove(id) != null;
     }
 
-    // Método para criar um novo produto
     public Product createProduct(Product product) {
         productRepository.put(product.getId(), product);
         return product;
